bf0dea7b3fdd8723134ecebfd87c0b3f7f0966bf
Zandrelius <zandreliusakain1@gmail.com>
2025-07-28 13:05:10 +0300

    B changes

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://task-y-frontend-4odj.vercel.app/"],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
     credentials: true,
   }),
